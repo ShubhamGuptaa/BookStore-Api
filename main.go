ccc673a8f33ea3ab7f214df3d555276cdee4cb7a
Shubham Kumar <58993830+ShubhamGuptaa@users.noreply.github.com>
2021-09-29 18:03:57 +0530

    Delete main.go

--- main.go
+++
@@ -1,7 +0,0 @@
-// Task: Create a bookinfo application in which user will provide book name and you will return book details
-// Create 3 microservices
-// Details Microservice  (return details from MongoDB based on Name)
-// Rating Microservice( return book rating from MongoDB based on Name)
-// Product Microservice (return list of all the books)
-// Create Restful Services and integrate the
-// Use MongoDB
